412a27eee893b490d82b6f34096e767dd369548b
DavidCabral <davidmeloc@gmail.com>
2016-10-08 21:10:22 -0300

    .

--- src/main/java/br/com/teste/Fachada.java
+++ src/main/java/br/com/teste/Fachada.java
@@ -11,36 +11,42 @@ import br.com.teste.service.AlmoxarifadoService;
 import br.com.teste.service.CategoriaService;
 
 @Service
-public class Fachada  {
-   @Autowired private AlmoxarifadoService almoxarifadoService;
-   
-   public Almoxarifado getAlmoxarifado(Integer id){
-	   return almoxarifadoService.getAlmoxarifado(id);
-   }
-	public List<Almoxarifado> listarAlmoxarifados(){
+public class Fachada {
+	@Autowired private AlmoxarifadoService almoxarifadoService;
+	@Autowired private CategoriaService categoriaService;
+
+	public Almoxarifado getAlmoxarifado(Integer id) {
+		return almoxarifadoService.getAlmoxarifado(id);
+	}
+
+	public List<Almoxarifado> listarAlmoxarifados() {
 		return almoxarifadoService.listarAlmoxarifados();
 	}
-	public void salvarAlmoxarifado(Almoxarifado almoxarifado){
+
+	public void salvarAlmoxarifado(Almoxarifado almoxarifado) {
 		almoxarifadoService.salvarAlmoxarifado(almoxarifado);
 	}
-	public void excluirAlmoxarifado(Almoxarifado almoxarifado){
+
+	public void excluirAlmoxarifado(Almoxarifado almoxarifado) {
 		almoxarifadoService.excluirAlmoxarifado(almoxarifado);
 	}
+
 	
-	
-	@Autowired private CategoriaService categoriaService;
-	   
-	   public Categoria getCategoriao(Integer id){
-		   return categoriaService.getCategoria(id);
-	   }
-		public List<Categoria> listarCategorias(){
-			return categoriaService.listarCategoria();
-		}
-		public void salvarCategoria(Categoria categoria){
-			categoriaService.salvarCategoria(categoria);
-		}
-		public void excluirCategoria(Categoria categoria){
-			categoriaService.excluirCategoria(categoria);
-		}
-   
+
+	public Categoria getCategoriao(Integer id) {
+		return categoriaService.getCategoria(id);
+	}
+
+	public List<Categoria> listarCategorias() {
+		return categoriaService.listarCategoria();
+	}
+
+	public void salvarCategoria(Categoria categoria) {
+		categoriaService.salvarCategoria(categoria);
+	}
+
+	public void excluirCategoria(Categoria categoria) {
+		categoriaService.excluirCategoria(categoria);
+	}
+
 }
